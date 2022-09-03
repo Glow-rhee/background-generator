f7d0f03ec986e8a01d8a70e9b1afcd19496848f2
glow-rhee <gloryalfredirabor047@gmail.com>
2022-09-03 13:13:00 +0100

    adding background generator

---
+++ script.js
@@ -0,0 +1,63 @@
+var css=document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body =document.getElementById("gradient");
+var randomButton = document.getElementById("Random");
+ function setGradient(){
+ 	body.style.background =
+"linear-gradient(to right, "
+ + color1.value
+  + "," 
+    +color2.value +
+    ")";	
+css.textContent =body.style.background +";"
+ }
+
+ function getRandomColor() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+	  color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+  }
+
+function randomColors(){
+	color1.value = getRandomColor();
+	color2.value = getRandomColor();
+
+	setGradient();
+}
+
+setGradient();
+
+
+
+randomButton.addEventListener("click", randomColors);
+
+color1.addEventListener("input",setGradient);
+color2.addEventListener("input", setGradient);
+
+
+//switch statement
+// function moveComman(direction){
+//   var whatHappens;
+//   switch(direction){
+//     case"forward":
+//     whatHappens ="you encounter a monster";
+//     break;
+//     case"back":
+//     whatHappens ="you arrived home";
+//     break;
+//     case"right":
+//     whatHappens ="you found a river";
+//     break;
+//     case"left":
+//     whatHappens ="you ran into a troll";
+//     break;
+//     default:
+//       whatHappens ="Please enter a valid direction";
+//   }
+//   return whatHappens;
+// }
+
